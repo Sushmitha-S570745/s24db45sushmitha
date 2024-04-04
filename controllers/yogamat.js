448b9389ac2e8ece0b41d8e0ee750428239a4605
Sushmitha-S570745 <sushmithareddyv5@gmail.com>
2024-04-03 20:05:40 -0500

    implement create

--- controllers/yogamat.js
+++ controllers/yogamat.js
@@ -42,6 +42,25 @@ exports.yogamat_list = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
- 
+
+    exports.yogamat_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new yogamat();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.material = req.body.material;
+    document.color = req.body.color;
+    document.thickness = req.body.thickness;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
     
     
